95a69d5f3d272aa8862b74cec2b828895c636afa
marcotsui2003 <marcotsui2003@yahoo.com>
2015-11-02 09:40:26 +0000

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,21 @@
-# Implement your object-oriented solution here!
+class SumSquareDifference
+  
+  def initialize(input)
+    @input = input 
+  end
+  
+  def difference
+    sum = 0
+    for i in (1..@input)
+      for j in (1..@input)
+        if i != j
+          sum += (i*j)
+        else
+        end
+      end
+    end
+    sum
+  end
+end
+
+    
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,13 @@
-# Implement your procedural solution here!
+def sum_square_difference(input)
+  sum = 0
+  for i in (1..input)
+    for j in (1..input)
+      if i != j
+        sum += (i*j)
+      else
+      end
+    end
+  end
+  sum
+end
+
